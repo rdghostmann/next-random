6c63747ebb049b49d7be0cbe302c64ad66c6c2f3
Rd <97230275+rdghostmann@users.noreply.github.com>
2024-04-01 05:12:33 +0100

    first commit

--- actions/scrape-randomNumbers.ts
+++ actions/scrape-randomNumbers.ts
@@ -1,36 +1,36 @@
 "use server";
 import puppeteer from "puppeteer";
-// import { revalidatePath } from "next/cache";
+import { revalidatePath } from "next/cache";
 
-export async function scrapeRandomNumbers(url:string) {
-    try {
-        const browser = await puppeteer.launch({headless: false});
-        const page = await browser.newPage();
-        const navigationPromise = page.waitForNavigation({
-            waitUntil: "networkidle0",
-            timeout: 120000,
-        });
+export async function scrapeRandomNumbers(url: string) {
+	try {
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		const navigationPromise = page.waitForNavigation({
+			waitUntil: "networkidle0",
+			timeout: 120000,
+		});
 
-        await page.goto(url, {waitUntil: "networkidle0", timeout: 120000});
-    ;
-        await navigationPromise;
+		await page.goto(url, { waitUntil: "networkidle0", timeout: 120000 });
 
- 
+		await navigationPromise;
 
-        const data = await page.evaluate(()=> {
-            const generatedNumber:any = document.querySelector('pre')?.innerText?.trim() || document.querySelector('.data')?.innerHTML;
-            // const generatedNumber:any = document.querySelector('pre')?.innerText?.trim() || document.querySelector('.data')?.innerHTML;
-            console.log(generatedNumber)
-            // const generatedNumber:any = $("pre.data").text().trim();
-            return generatedNumber;
-        })
+		const data = await page.evaluate(() => {
+			const generatedNum: any = document.querySelector('pre')?.innerHTML?.trim().split('\n');
 
-        // await browser.close();
-        // revalidatePath('/');
-        return {...data, url};
-    } catch (error) {
-        console.log(error);
-        return null;
-        
-    }
+			console.log("StoreDB", { generatedNum })
+			return {generatedNum};
+
+
+		})
+
+		// await browser.close();
+		// revalidatePath('/');
+		return { ...data };
+
+	} catch (error) {
+		console.log(error);
+		return null;
+
+	}
 }
